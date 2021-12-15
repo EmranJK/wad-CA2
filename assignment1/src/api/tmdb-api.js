8afc0fb348cf01c3c10e89209cdc5020f4926830
Emran Sabbagh <emran2156@gmail.com>
2021-12-15 13:30:38 +0000

    Errors

--- assignment1/src/api/tmdb-api.js
+++ assignment1/src/api/tmdb-api.js
@@ -1,7 +1,8 @@
 export const getMovies = () => {
   return fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-  ).then((response) => {
+    
+    ).then((response) => {
     if (!response.ok) {
       throw new Error(response.json().message);
     }
@@ -16,8 +17,8 @@ export const getMovies = () => {
 ////////////
 export const getUpcomingMovies = () => {
   return fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-
+    //`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    `http://localhost:8080/api/movies/tmdb/upcoming`
     //'https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1'
    ).then((response) => {
     if (!response.ok) {
